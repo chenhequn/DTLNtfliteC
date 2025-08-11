cc6d26377ae257bebc446eee7bc1d72c290d92ff
陈鹤群 <chen.hequn@ghtchina.com>
2025-08-11 11:29:01 +0800

    change dtln after ns

--- src/dios_ssp_api.c
+++ src/dios_ssp_api.c
@@ -267,13 +267,6 @@ int dios_ssp_process_api(void* ptr, short* mic_buf, short* ref_buf, short* out_b
     *pvad = srv->vad_result = dios_ssp_vad_result_get(srv->ptr_vad);
 #endif
 
-    if(SSP_PARAM->DTLN_KEY == 1) {
-        ret = dios_ssp_dtln_process(srv->ptr_dtln, srv->ptr_data_buf);
-        if(ret != 0) {
-            return ERROR_DTLN;
-        }
-    }
-
     // ns process
     if(SSP_PARAM->NS_KEY == 1) {
         ret = dios_ssp_ns_process(srv->ptr_ns, srv->ptr_data_buf);
@@ -281,6 +274,14 @@ int dios_ssp_process_api(void* ptr, short* mic_buf, short* ref_buf, short* out_b
             return ERROR_NS;
         }
     }
+
+    if(SSP_PARAM->DTLN_KEY == 1) {
+        ret = dios_ssp_dtln_process(srv->ptr_dtln, srv->ptr_data_buf);
+        if(ret != 0) {
+            return ERROR_DTLN;
+        }
+    }
+
 
 #if 1
     // vad process
